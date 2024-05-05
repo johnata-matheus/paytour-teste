a5b5d50ade51649e83cd1762a43f1b9347dcbda5
Johnata Matheus <matheusabdala12@gmail.com>
2024-05-04 22:06:41 -0300

    feat: create user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nome',
         'email',
-        'password',
+        'telefone',
+        'cargo_desejado',
+        'escolaridade',
+        'data_envio',
+        'observacoes',
+        'arquivo'
     ];
 
     /**
